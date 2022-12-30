187d933faee3418057058346495ec57d6080ec5a
lealalves <leealalves@hotmail.com>
2022-12-30 16:17:56 -0300

    config for express-session

--- src/server.js
+++ src/server.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const app = express()
+const session = require('express-session')
 
 const authController = require('./controller/authController')
+
+app.use(session({
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: true, 
+  cookie: { 
+    maxAge: 60000,
+    sameSite: 'none',
+    secure: false
+   }
+}))
 
 app.use(express.json())
 
 app.use('/auth', authController)
 
+app.use((req, res, next) => {
+  if(!req.session.isLogged){
+    return console.log('pls loggin');
+  }
+  next()
+})
+
+
 app.get('/', (req, res) => {
-  console.log(req.isLogged);
   res.send('Oi!')
 })
 
