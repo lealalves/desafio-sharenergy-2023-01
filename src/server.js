2210e04b2b63b0b09b278b9be8d0bb9ddcc6a2ca
lealalves <leealalves@hotmail.com>
2023-01-04 18:02:52 -0300

    added and config cors module

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 const session = require('express-session')
+const cors = require('cors')
 
 const authController = require('./controller/authController')
 const servicesController = require('./controller/servicesController')
 const clientController = require('./controller/clientController')
+
+
+const corsOptions = {
+  origin: 'http://localhost:5173',  //Your Client, do not write '*'
+  credentials: true,
+};
+app.use(cors(corsOptions))
 
 app.use(session({
   secret: 'keyboard cat', 
@@ -12,7 +20,6 @@ app.use(session({
   saveUninitialized: true, 
   cookie: { 
     maxAge: 60000,
-    sameSite: 'none',
     secure: false
    }
 }))
@@ -25,13 +32,13 @@ app.use('/client', clientController)
 
 app.use((req, res, next) => {
   if(!req.session.isLogged){
-    return res.send('login')
+    return res.send({message: 'need login'})
   }
   next()
 })
 
 app.get('/', (req, res) => {
-  res.send('Oi!')
+  res.send({message: 'Home'})
 })
 
 app.listen(3000, () => {
