482f6ec3865a394cf3f86da71998d59b87ed2cb6
lealalves <leealalves@hotmail.com>
2023-01-06 19:23:44 -0300

    added header and mobile menu

---
+++ frontend/src/components/Header/Dropdown.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="dropdown">
+    <li @click="isOpen = !isOpen">Menu</li>
+      <div class="sub-menu" v-show="isOpen" >
+        <div v-for="(item, i) in items" :key="i">
+          <component :is="item.is" :to="item.url">{{item.title}}</component>
+        </div>
+      </div>
+  </div>
+</template>
+<style scoped>
+.sub-menu {
+  z-index: 1;
+  position: absolute;
+  background-color: #222;
+  right: -10px;
+  border-radius: 10px;
+}
+.sub-menu div{
+  padding: 10px;
+  text-align: center;
+  cursor: pointer;
+}
+li{
+  padding: 5px;
+}
+a{
+  color: #f5f5f5;
+}
+</style>
+<script>
+  export default {
+    name: 'Dropdown',
+    props: ['items'],
+    data() {
+		return {
+			isOpen: false
+		}
+	},
+}
+</script>
---
+++ frontend/src/components/Header/Header.vue
@@ -0,0 +1,93 @@
+<template>
+  <header>
+        <nav>
+          <img id="img-logo" src="../../assets/logo.jpg" alt="Logo"/>
+          <ul id="desktop-menu">
+            <li>Home</li>
+            <li>HTTP Cat</li>
+            <li>Dogs</li>
+            <li>Clientes</li>
+          </ul>  
+          <Dropdown id="mobile-menu" :items="header"/>
+        </nav>
+      </header>
+</template>
+<style scoped>
+#mobile-menu{
+  display: none;
+}
+#img-logo{
+  border: 1px solid #000;
+  width: min(70px, 12vw);
+  border-radius: 10px;
+}
+header{
+  display: flex;
+  padding: 15px;
+  width: 100%;
+  background-color: tomato;
+  margin-bottom: 30px;
+}
+nav{
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: flex-start
+}
+ul{
+  display: flex;
+}
+li{
+  cursor: pointer;
+  padding: 10px 15px 10px 15px;
+}
+
+
+@media (max-width: 768px) {
+  nav{
+    justify-content: space-between;
+  }
+  #desktop-menu{
+    display: none;
+  }
+  #mobile-menu{
+    display: initial;
+  }
+}
+</style>
+<script>
+import Dropdown from './Dropdown.vue';
+
+export default {
+  name: 'Header',
+  components:{
+    Dropdown
+  },
+  data(){
+    return {
+      header: [
+				{
+					title: 'Home',
+					url: '/home',
+					is: 'router-link',
+				},
+				{
+					title: 'HTTP Cat',
+					url: '/home/httpcat',
+					is: 'router-link',
+				},
+				{
+					title: 'Dogs',
+					url: '/home',
+					is: 'router-link',
+				},
+				{
+					title: 'Clientes',
+					url: '/home',
+					is: 'router-link',
+				},
+			]
+    }
+  }
+}
+</script>
