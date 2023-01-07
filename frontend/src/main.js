a63eb251e3c3da8f2fbd75f93b14fc6d64e12537
lealalves <leealalves@hotmail.com>
2023-01-07 19:14:27 -0300

    added v-pagination-3

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,11 @@ import router from './router'
 
 import './assets/main.css'
 
+import Pagination from 'v-pagination-3';
+
 const app = createApp(App)
+
+app.component('pagination', Pagination);
 
 app.use(router)
 
