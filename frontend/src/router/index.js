128ae5f96f513c3e68818c7c1974b62fbb014ba4
lealalves <leealalves@hotmail.com>
2023-01-07 21:31:02 -0300

    added http cat view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 import Home from '../views/Home.vue'
+import Cat from '../views/Cat.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/home/httpcat',
+      name: 'httpcat',
+      component: Cat
     }
   ]
 })
