8eed6e0bfc628e1c09f69040bb5250c9e6c7ec05
lealalves <leealalves@hotmail.com>
2023-01-10 15:41:28 -0300

    new message component

---
+++ frontend/src/components/Message.vue
@@ -0,0 +1,27 @@
+<template>
+	<main>
+    <div id="container-mensagem">
+      <p>{{ msg }}</p>
+    </div>
+	</main>
+</template>
+<style scoped>
+#container-mensagem{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 7vw;
+  margin-bottom: 10px;
+  color: #adff2f;
+}
+</style>
+<script>
+export default {
+  name: 'Message',
+  props: ['msg'],
+  data(){
+    return {
+    }
+  }
+}
+</script>
