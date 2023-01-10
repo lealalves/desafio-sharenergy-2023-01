50052bcac9efd609ed396541d4951dcce5be9e4d
lealalves <leealalves@hotmail.com>
2023-01-10 01:55:39 -0300

    added route for clients view

--- frontend/src/components/Header/Header.vue
+++ frontend/src/components/Header/Header.vue
@@ -82,7 +82,7 @@ export default {
 				},
 				{
 					title: 'Clientes',
-					url: '/home',
+					url: '/clientes',
 					is: 'router-link',
 				},
 			]
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,9 @@ import Login from '../views/Login.vue'
 import Home from '../views/Home.vue'
 import Cat from '../views/Cat.vue'
 import Dog from '../views/Dog.vue'
+import Clientes from '../views/Clientes/Clientes.vue'
+import Cadastro from '../views/Clientes/Cadastro.vue'
+import Editar from '../views/Clientes/Editar.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +29,21 @@ const router = createRouter({
       path: '/dogs',
       name: 'dogs',
       component: Dog
+    },
+    {
+      path: '/clientes',
+      name: 'clientes',
+      component: Clientes
+    },
+    {
+      path: '/clientes/cadastro',
+      name: 'clientesCadastro',
+      component: Cadastro
+    },
+    {
+      path: '/clientes/editar/:id',
+      name: 'clientesEditar',
+      component: Editar,
     }
   ]
 })
